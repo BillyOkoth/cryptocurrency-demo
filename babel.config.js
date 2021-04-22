ee8b8b31d7ba93a5dad3745c9d4a615a056091df
HenryToti <henryokoth3@gmail.com>
2021-04-22 17:16:23 +0300

    1.assets api using rxjs fromfetch.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
-    presets:["@babel/preset-env",["@babel/preset-react",{runtime:"automatic"}]]
+    presets:["@babel/preset-env",["@babel/preset-react",{runtime:"automatic"}]],
+
     ///automatically imports the jsx functions that are going to be transpiled.
+    "plugins": [
+        [
+          "@babel/plugin-transform-runtime",
+          {
+            "absoluteRuntime": false,
+            "corejs": false,
+            "helpers": true,
+            "regenerator": true,
+            "version": "^7.13.17"
+          }
+        ]
+      ]
+      //the pluggin allows to cater for async function . installation ^7.13.17 plugin transform runtime.
+
 }
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,82 @@
+import { useEffect, useState } from "react";
+import { fromFetch } from "rxjs/fetch";
+import { Table, Tag, Space } from "antd";
+import "./app.css";
 
-const App = ()=>{
-    return(
-        <div>Hello Billy</div>
+let requestOptions = { method: "GET", redirect: "follow" };
+
+const dataAssets$ = fromFetch(
+  "https://api.coincap.io/v2/assets",
+  requestOptions
+);
+const columns = [
+  {
+    title: "Rank",
+    dataIndex: "rank",
+    key: "rank",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Market Cap",
+    dataIndex: "marketCapUsd",
+    key: "marketCapUsd",
+  },
+  {
+    title: "Market Cap",
+    dataIndex: "marketCapUsd",
+    key: "marketCapUsd",
+  },
+
+  {
+    title: "VWAP 24hrs",
+    dataIndex: "vwap24Hr",
+    key: "vwap24Hr",
+  },
+  {
+    title: "Supply",
+    dataIndex: "supply",
+    key: "supply",
+  },
+  {
+    title: "Volume 24hrs",
+    dataIndex: "volumeUsd24Hr",
+    key: "volumeUsd24Hr",
+  },
+  {
+    title: "Change",
+    dataIndex: "changePercent24Hr",
+    key: "changePercent24Hr",
+  },
+];
+
+const App = () => {
+  const [assets, setAssets] = useState([]);
+  console.log(assets);
+  useEffect(async () => {
+    dataAssets$.subscribe((res) =>
+      res.json().then((data) => setAssets(data.data))
     );
 
-}
+    return () => {
+      dataAssets$.unsubscribe();
+    };
+  }, []);
+  return (
+    <div>
+      <Table
+        rowKey="id"
+        columns={columns}
+        dataSource={assets}
+        bordered={true}
+        scroll={{ y: 500 }}
+        pagination={false}
+      />
+    </div>
+  );
+};
 export default App;
