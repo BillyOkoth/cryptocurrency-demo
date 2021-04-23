73cf43ccab3922cff227b96dc9c84e2d65ed955d
HenryToti <henryokoth3@gmail.com>
2021-04-23 11:44:40 +0300

    1.design of the overal layout of the application.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 import { fromFetch } from "rxjs/fetch";
 import { webSocket } from "rxjs/webSocket";
-import { Table, Tag, Space } from "antd";
+import { Table, Tag, Input, Layout, Row, Col, Divider, Card, Menu } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  BarChartOutlined,
+  DollarOutlined,
+} from "@ant-design/icons";
 import "./app.css";
 
+const { Header, Footer, Sider, Content } = Layout;
+const { Search } = Input;
 let requestOptions = { method: "GET", redirect: "follow" };
 
 const dataAssets$ = fromFetch(
@@ -58,7 +66,6 @@ const columns = [
   },
 ];
 
-
 const App = () => {
   const [assets, setAssets] = useState([]);
   useEffect(async () => {
@@ -66,26 +73,98 @@ const App = () => {
       res.json().then((data) => setAssets(data.data))
     );
 
-    websocketObservable$.subscribe(
-      (res)=>console.log('message received',res)
-    )
+    websocketObservable$.subscribe((res) =>
+      console.log("message received", res)
+    );
 
     return () => {
       dataAssets$.unsubscribe();
       websocketObservable$.unsubscribe();
     };
   }, []);
-  
+
   return (
-    <div>
-      <Table
-        rowKey="id"
-        columns={columns}
-        dataSource={assets}
-        bordered={true}
-        scroll={{ y: 500 }}
-        pagination={false}
-      />
+    <div style={{ height: "100vh", overflowY: "hidden" }}>
+      <Layout>
+        <Header style={{ backgroundColor: "#ffffff" }}>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={12}>
+              <Menu mode="horizontal" style={{ marginLeft: "-2.5em" }}>
+                <Menu.Item key="mail" icon={<DollarOutlined />}>
+                  Coins
+                </Menu.Item>
+                <Menu.Item key="app" icon={<AppstoreOutlined />}>
+                  Exchanges
+                </Menu.Item>
+                <Menu.Item key="alipay" icon={<BarChartOutlined />}>
+                  Charts
+                </Menu.Item>
+              </Menu>
+            </Col>
+            <Col className="gutter-row" span={12} >
+              <Search style = {{marginTop:"1em"}} placeholder="input search text" enterButton />
+            </Col>
+          </Row>
+        </Header>
+        <Content style={{ margin: "2em" }}>
+          <Row
+            gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+            style={{ marginBottom: "1em" }}
+          >
+            <Col className="gutter-row" span={4}>
+              <div>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </div>
+            </Col>
+            <Col className="gutter-row" span={4}>
+              <div>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </div>
+            </Col>
+            <Col className="gutter-row" span={4}>
+              <div>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </div>
+            </Col>
+            <Col className="gutter-row" span={4}>
+              <div>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </div>
+            </Col>
+            <Col className="gutter-row" span={4}>
+              <div>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </div>
+            </Col>
+            <Col className="gutter-row" span={4}>
+              <div>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </div>
+            </Col>
+          </Row>
+          <Table
+            rowKey="id"
+            columns={columns}
+            dataSource={assets}
+            bordered={true}
+            scroll={{ y: 400 }}
+            pagination={false}
+          />
+        </Content>
+        {/* <Footer>Footer</Footer> */}
+      </Layout>
     </div>
   );
 };
