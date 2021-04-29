4ba1f50506f2d2558484c21f701340add16e79d5
HenryToti <henryokoth3@gmail.com>
2021-04-29 21:47:20 +0300

    1.implementation of search using rxjs .

--- src/components/coins/coins.js
+++ src/components/coins/coins.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef} from "react";
+import { BehaviorSubject} from "rxjs";
 import { fromFetch } from "rxjs/fetch";
 import { webSocket } from "rxjs/webSocket";
-import { Table } from 'antd';
+import { Table, Input, Row, Col,Alert } from 'antd';
+import { filter,debounceTime,mergeMap,distinctUntilChanged } from "rxjs/operators";
 
 let requestOptions = { method: "GET", redirect: "follow" };
+const { Search } = Input;
 const columns = [
   {
     title: "Rank",
@@ -55,9 +58,7 @@ const columns = [
         {
           maximumFractionDigits: 2,
           minimumFractionDigits: 2,
-        }).format(marketCapFloatTrillion);
-
-      console.log(typeof (renderValue))
+        }).format(marketCapFloatTrillion);     
       return <span>{renderValue}m</span>;
 
     }
@@ -85,7 +86,6 @@ const columns = [
     }
   },
 ];
-
 const dataAssets$ = fromFetch(
   "https://api.coincap.io/v2/assets",
   requestOptions
@@ -94,33 +94,90 @@ const websocketObservable$ = webSocket(
   "wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero,litecoin"
 );
 
-
-const Coins = () => {
+const Coins = () => {  
   const [assets, setAssets] = useState([]);
+  const [filteredAssets, setFilteredAssets] = useState([]);
+  const [search, setSearch] = useState(''); 
+  let searchSubject$ = new BehaviorSubject(''); 
+
+  const handleChange = (e)=>{
+    const newValue = e.target.value;
+    setSearch(newValue.toLowerCase());
+    searchSubject$.next(newValue.toLowerCase()); 
+  }  
+  const filterAssets = async term =>{    
+    const searchResult = await assets.filter(asset=>asset.id.includes(term));  
+    return searchResult;
+  }
+
+  let searchObservable = searchSubject$.pipe(
+    filter(val=>val.length>1),
+    debounceTime(750),
+    distinctUntilChanged(),
+    mergeMap(val=>filterAssets(val))
+  );
+
+
   useEffect(async () => {
     dataAssets$.subscribe((res) =>
-      res.json().then((data) => setAssets(data.data))
-    );
-
-    websocketObservable$.subscribe((res) =>
-      console.log("message received", res)
-    );
+      res.json().then((data) => {
+        setAssets(data.data),
+        setFilteredAssets(data.data)
+      }
+          
+      )
+    );   
+    
+    // websocketObservable$.subscribe((res) =>
+    //   console.log("message received", res)
+    // );
 
     return () => {
+      //websocket not unsubscribing!
       dataAssets$.unsubscribe();
-      websocketObservable$.unsubscribe();     
+      websocketObservable$.unsubscribe();
+
     };
   }, []);
-  return (
+   
+  useEffect(() => {
+    if(!search){
+      setFilteredAssets(assets);
+    }else{
+      searchObservable.subscribe((result)=>{
+        setFilteredAssets(result);
+       })
+    }
+    return () => {
+      searchObservable.unsubscribe();
+    }
+  }, [searchObservable,setFilteredAssets])
+  
+ return (
+    
     <div>
-      <Table
-        rowKey="id"
-        columns={columns}
-        dataSource={assets}
-        bordered={true}
-        scroll={{ y: 400 }}
-        pagination={false}
-      />
+      <div>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          <Col className="gutter-row" span={24} >
+            <Search style={{ marginTop: "1em" }}  onChange = {handleChange} value = {search} placeholder="Search" enterButton />
+          </Col>
+        </Row>
+      </div>
+      <div style = {{marginTop:"2em"}}>
+        {
+          !filteredAssets.length ?(<Alert message="No Data Available!" type="error" />):(
+            <Table
+          rowKey="id"
+          columns={columns}
+          dataSource={filteredAssets}
+          bordered={true}
+          scroll={{ y: 400 }}
+          pagination={false}
+        />
+          )
+        }
+        
+      </div>
     </div>
   );
 
--- src/components/exchanges/exchanges.js
+++ src/components/exchanges/exchanges.js
@@ -1,89 +1,143 @@
 import { useEffect, useState } from "react";
 import { fromFetch } from "rxjs/fetch";
-import {Table} from 'antd';
+import { Table, Row, Col, Input,Alert } from 'antd';
+import { BehaviorSubject } from "rxjs";
+import { debounceTime, distinctUntilChanged, filter, mergeMap } from "rxjs/operators";
+
 
 let requestOptions = { method: "GET", redirect: "follow" };
+const { Search } = Input;
 const columns = [
-    {
-      title: "Rank",
-      dataIndex: "rank",
-      key: "rank",
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Trading Pairs",
-      dataIndex: "tradingPairs",
-      key: "tradingPairs",
-    },  
-    {
-      title: "Volume 24hrs",
-      dataIndex: "volumeUsd",
-      key: "volumeUsd",
-      render:value=>{
-        let volumeUsdFloat= parseFloat(value)/1.0e8;
-        let renderValue = volumeUsdFloat.toFixed(2);
-      return<span>{renderValue}B</span>
+  {
+    title: "Rank",
+    dataIndex: "rank",
+    key: "rank",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Trading Pairs",
+    dataIndex: "tradingPairs",
+    key: "tradingPairs",
+  },
+  {
+    title: "Volume 24hrs",
+    dataIndex: "volumeUsd",
+    key: "volumeUsd",
+    render: value => {
+      let volumeUsdFloat = parseFloat(value) / 1.0e8;
+      let renderValue = volumeUsdFloat.toFixed(2);
+      return <span>{renderValue}B</span>
 
-      }
-    },
-    {
-      title: "Total(%)",
-      dataIndex: "percentTotalVolume",
-      key: "percentTotalVolume",
-      render:(value)=>{
-        let totalFloat = parseFloat(value);
-        let renderValue = totalFloat.toFixed(2);
-        return <span>{renderValue}%</span>
-      }
-    },
-    {
-        title: "Status",
-        dataIndex: "socket",
-        key: "socket",
-      },
-    
-  ];
+    }
+  },
+  {
+    title: "Total(%)",
+    dataIndex: "percentTotalVolume",
+    key: "percentTotalVolume",
+    render: (value) => {
+      let totalFloat = parseFloat(value);
+      let renderValue = totalFloat.toFixed(2);
+      return <span>{renderValue}%</span>
+    }
+  },
+  // {
+  //   title: "Status",
+  //   dataIndex: "socket",
+  //   key: "socket",
+  // },
+];
+
 
 const dataAssets$ = fromFetch(
-    "https://api.coincap.io/v2/exchanges",
-    requestOptions
-  );
-const Exchanges = () =>{
-    const [assets, setAssets] = useState([]);
-    useEffect(async () => {
-      dataAssets$.subscribe((res) =>
-        res.json().then((data) => setAssets(data.data))
-      );
-  
-      websocketObservable$.subscribe((res) =>
-        console.log("message received", res)
-      );
-  
-      return () => {
-        dataAssets$.unsubscribe();
-        websocketObservable$.unsubscribe();
-       
-      };
-    }, []);
+  "https://api.coincap.io/v2/exchanges",
+  requestOptions
+);
+//filterfunction. 
 
-    return(
-        <div>
+const Exchanges = () => {
+  const [assets, setAssets] = useState([]);
+  const [filteredExchanges, setFilteredExchanges] = useState([]);
+  const [search, setSearch] = useState('');
+  
+  let searchSubject$ = new BehaviorSubject('');
+  //filterfunction
+  const filterExchanges = async term =>{
+      
+    const exchangesResult = await assets.filter(asset=> asset.exchangeId.includes(term));   
+    return exchangesResult;
+  }  
+  //onchange function.
+  const handleChange = (e)=>{
+    const newValue = e.target.value;
+    setSearch(newValue.toLowerCase());
+    searchSubject$.next(newValue.toLowerCase()); 
+  }
+  //observable
+  let searchObservable = searchSubject$.pipe(
+    filter(val=>val.length>1),
+    debounceTime(750),
+     distinctUntilChanged(),
+     mergeMap(val=> filterExchanges(val))   
+  )
+
+  useEffect(async () => {
+    dataAssets$.subscribe((res) =>
+      res.json().then((data) => {
+        setAssets(data.data)
+        setFilteredExchanges(data.data)
+      })
+    );   
+    return () => {
+      dataAssets$.unsubscribe();
+      websocketObservable$.unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+      if(!search){
+        setFilteredExchanges(assets);
+      }else {
+        searchObservable.subscribe((result) =>{
+         
+          setFilteredExchanges(result);
+        })
+      }
+    return () => {
+      searchObservable.unsubscribe()
+    }
+  }, [searchObservable,setFilteredExchanges])
+  return (
+    <div>
+      <div>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          <Col className="gutter-row" span={24} >
+            <Search style={{ marginTop: "1em" }} placeholder="Search" value = {search} onChange = {handleChange} enterButton />
+          </Col>
+        </Row>
+      </div>
+      <div style={{ marginTop: "2em" }}>
+        {
+          !filteredExchanges.length ? (
+            <Alert message="No Data Available!" type="error" />
+          ):(
             <Table
-             rowKey="id"
-             columns={columns}
-             dataSource={assets}
-             bordered={true}
-             scroll={{ y: 400 }}
-             pagination={false}
-           />
-            
-        </div>
-    );
+            rowKey="exchangeId"
+            columns={columns}
+            dataSource={filteredExchanges}
+            bordered={true}
+            scroll={{ y: 400 }}
+            pagination={false}
+          />
+          )
+        }  
+      </div>
+    </div>
+  );
 }
 export default Exchanges;
 
